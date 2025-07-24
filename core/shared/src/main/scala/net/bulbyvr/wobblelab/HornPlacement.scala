d61a2550200a6f097728c61910064e54e8c6753b
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2025-07-24 11:41:22 -0400

    Rename HornPlacement.None display Standard => No Horns

--- core/shared/src/main/scala/net/bulbyvr/wobblelab/HornPlacement.scala
+++ core/shared/src/main/scala/net/bulbyvr/wobblelab/HornPlacement.scala
@@ -2,7 +2,7 @@ package net.bulbyvr
 package wobblelab
 
 enum HornPlacement(val display: String) {
-  case None extends HornPlacement("Standard")
+  case None extends HornPlacement("No Horns")
   case Center extends HornPlacement("Center")
   case Traditional extends HornPlacement("Traditional")
 }
